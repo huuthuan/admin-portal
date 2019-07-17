0d3ffbb0fa7ed84ccc1f11d1872a0db83b6bf622
thuanhuu <lehuuthuan292@gmail.com>
2019-07-18 01:30:13 +0700

    Page staff

--- src/app/components/md-admin/staffs/staffs.component.ts
+++ src/app/components/md-admin/staffs/staffs.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Staff} from '@app/models';
+import {StaffService} from '@app/services';
 
 @Component({
   selector: 'app-staffs',
@@ -6,11 +8,60 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./staffs.component.scss']
 })
 export class StaffsComponent implements OnInit {
+  dataSource: Staff[] = [];
+  isStaffPopup: boolean = false;
+  staffPopupTitle = 'New Insurance';
+  selectedStaff: Staff;
+  isOpenMenu: boolean = false;
 
-  constructor() {
+  constructor(private staffService: StaffService) {
   }
 
   ngOnInit() {
+    this.loadStaffs();
+  }
+
+  loadStaffs() {
+    this.dataSource = this.staffService.getAll();
+  }
+
+  onAddStaff() {
+    this.staffPopupTitle = 'Add New Staff';
+    this.selectedStaff = new Staff();
+    this.isStaffPopup = true;
+  }
+
+  onEditStaffTemplate(staff) {
+    this.staffPopupTitle = 'Edit Staff';
+    this.selectedStaff = staff;
+    this.isStaffPopup = true;
+  }
+
+  onSavedStaff() {
+    this.isStaffPopup = false;
+    this.loadStaffs();
+  }
+
+  onDeleteStaffTemplate(staff: Staff) {
+
+  }
+
+  onCancelStaff() {
+    this.isStaffPopup = false;
+  }
+
+  onToggleMenu() {
+    this.isOpenMenu = !this.isOpenMenu;
+  }
+
+  onCloseMenu() {
+    if (this.isOpenMenu) {
+      this.isOpenMenu = false;
+    }
+  }
+
+  onLogout() {
+
   }
 
 }
